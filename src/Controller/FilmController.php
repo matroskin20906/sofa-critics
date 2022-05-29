004a6e13208502aa5ae9e32a5dac8629e3a8b5c3
Boriss <borisliepins@gmail.com>
2022-05-29 15:46:54 +0300

    Html backup

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -8,15 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FilmController extends AbstractController
 {
-    #[Route('/', name: 'welcome', methods: ['GET'])]
+    #[Route('/', name: 'app_welcome', methods: ['GET'])]
     public function welcome(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('home\homepage.html.twig');
     }
 
-    #[Route('/film', name: 'film_page', methods: ['GET'])]
+    #[Route('/film', name: 'app_filmpage', methods: ['GET'])]
     public function id(): Response
     {
-        return $this->render('film.html.twig');
+        return $this->render('film\filmpage.html.twig');
     }
 }
